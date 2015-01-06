fba38288314f004fe66308d0559b8b305dcbadd9
Matt Fullerton <matt.fullerton@gmail.com>
2015-01-06 14:57:57 +0100

    Use latest tika-server, add tesseract

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ MAINTAINER Todd Tyree <tatyree@gmail.com>
 RUN mkdir /setup
 ADD install.sh /setup/install.sh
 RUN /setup/install.sh
-ENTRYPOINT java -jar /srv/tika-server-1.6.jar -host 0.0.0.0
+ENTRYPOINT java -jar /srv/tika-server-1.8-SNAPSHOT.jar -host 0.0.0.0
 
 EXPOSE 9998
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
+echo "# Installing Maven"
+apt-get update
+apt-get -y -q install default-jdk maven unzip
+
 echo "# Installing Tika"
-curl http://www.apache.org/dist/tika/tika-server-1.6.jar -s -o /srv/tika-server-1.6.jar
+mkdir install
+curl https://codeload.github.com/apache/tika/zip/trunk -o trunk.zip
+unzip trunk.zip
+cd tika-trunk
+mvn clean install
+cp tika-server/target/tika-server-1.8-SNAPSHOT.jar /srv/tika-server-1.8-SNAPSHOT.jar
+
+echo "#Installing tesseract"
+apt-get -y -q install tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng
 
 echo "# Cleaning up"
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /setup /build
