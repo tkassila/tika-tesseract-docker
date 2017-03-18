88fc9edb7005352ca2c42266f586cb29d4cbfd2d
tkassila <tuomas.kassila@gmail.com>
2017-03-18 11:08:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mvn -DskipTests=true clean install
 cp tika-server/target/tika-server-1.*.jar /srv/tika-server-1.*.jar
 
 echo "#Installing tesseract"
-apt-get -y -q install tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng  tesseract-ocr-fin
+apt-get -y -q install tesseract-ocr tesseract-ocr-swe tesseract-ocr-eng  tesseract-ocr-fin
 
 echo "# Cleaning up"
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /setup /build
